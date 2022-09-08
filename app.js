6aed32743e812c2a3912a9e264d85e1fa863c4ff
Asharib Ali <96954816+Ashrib@users.noreply.github.com>
2022-09-08 14:58:22 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -13,44 +13,52 @@ bodyDiv.appendChild(inputsBox);
 
 var input = document.createElement("input");
 input.setAttribute("type", "text");
+input.setAttribute("placeholder", "Add your new todo");
 input.setAttribute("id", "user-input");
 inputsBox.appendChild(input);
 
 var submit_btn = document.createElement("input");
 submit_btn.setAttribute("type", "button");
+submit_btn.setAttribute("id", "submit-btn");
 submit_btn.setAttribute("value","Submit");
 submit_btn.setAttribute("onClick","submit()");
 inputsBox.appendChild(submit_btn);
 
+var allTodoDiv = document.createElement("div");
+allTodoDiv.setAttribute("id", "all-todoBox")
+bodyDiv.appendChild(allTodoDiv);
+
 var arrayOfData = [];
 function submit() {
     var getValue= input.value;
+    getValue = getValue.replace(/\s\s+/g , " ");
 
-    function Task(id , userInput){
-        this.id = id;
-        this.userInput = userInput, 
-        this.completed = false
+    if(getValue === "" || getValue === " ") {
+        alert("Please fill the input correctly!")
     }
-    var obj = new Task(arrayOfData.length, getValue);
-    arrayOfData.push(obj);
-
-    console.log(arrayOfData);
-    
-    var objDiv = document.createElement("div");
-    bodyDiv.appendChild(objDiv);
-    var taskDiv = document.createElement("div");
-    for(var i=0; i<arrayOfData.length; i++) {
-        taskDiv.setAttribute("id", i)
-        for(var key in arrayOfData[i]){
-    console.log(arrayOfData[i][key]);
-            
-           // taskDiv.appendChild(document.createTextNode(arrayOfData[i].userInput))
+    else{
+        function Task(id , userInput, completed){
+            this.id = id;
+            this.userInput = userInput; 
+            this.completed = completed;
         }
+        var todo = new Task(arrayOfData.length, getValue, false);
+        arrayOfData.push(todo);
+        console.log(arrayOfData);
         
-        
+        var taskDiv = document.createElement("div");
+        for(var i=0; i<arrayOfData.length; i++) {
+            taskDiv.setAttribute("id", i)
+            for(var key in arrayOfData[i]){
+                console.log(arrayOfData[i][key]);
+                
+                taskDiv.appendChild(document.createTextNode(arrayOfData[i][key]))
+            }
+            
+        }
+        allTodoDiv.appendChild(taskDiv);
+    
+        document.getElementById("user-input").value = null;
     }
-    objDiv.appendChild(taskDiv);
-
-    document.getElementById("user-input").value = null;
 }
 
